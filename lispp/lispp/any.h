1cdf11594948d7fd9476ef5344075f174f73675a
litleo97 <litleo@mail.ru>
2017-11-13 20:07:53 +0300

    added any.h

---
+++ lispp/lispp/any.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <typeinfo>
+
+struct PlaceHolder {
+    virtual const std::type_info& TypeInfo() const = 0;
+    virtual std::unique_ptr<PlaceHolder> Clone() const = 0;
+    virtual ~PlaceHolder() {};
+};
+
+template <class T>
+struct Holder: public PlaceHolder {
+    Holder(const T& value): held{value} {}
+
+    std::unique_ptr<PlaceHolder> Clone() const override {
+        return std::make_unique<Holder<T>>(held);
+    }
+
+    const std::type_info& TypeInfo() const override {
+        return typeid(held);
+    }
+
+    T held;
+};
+
+class Any {
+public:
+    Any() {}
+
+    template<class T>
+    Any(const T& value): content_{std::make_unique<Holder<T>>(value)} {}
+
+    template<class T>
+    Any& operator=(const T& value) {
+        content_ = std::make_unique<Holder<T>>(value);
+        return *this;
+    }
+
+    Any(const Any& rhs):
+            content_{!rhs.Empty() ?
+                     rhs.content_->Clone() :
+                     std::unique_ptr<PlaceHolder>(nullptr)}
+    {}
+
+    Any& operator=(Any rhs) {
+        Swap(rhs);
+        return *this;
+    }
+
+    ~Any() {
+        content_.reset();
+    }
+
+    void Swap(Any& rhs) {
+        content_.swap(rhs.content_);
+    }
+
+    template<class T>
+    const T& GetValue() const {
+        if (typeid(T) != content_->TypeInfo()) {
+            std::cout << "Wrong any cast!\n";
+        }
+
+        auto item = static_cast<Holder<T>*>(content_.get());
+        return item->held;
+    }
+
+    bool Empty() const {
+        return !content_;
+    }
+
+    void Clear() {
+        Any().Swap(*this);
+    }
+
+private:
+    std::unique_ptr<PlaceHolder> content_;
+};
